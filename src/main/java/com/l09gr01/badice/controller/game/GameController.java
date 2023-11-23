f7254fe06d22c60d8e4a208a18dd9ade4c60354a
joaocordeiro7 <up202205682@up.pt>
2023-11-23 16:04:57 +0000

    Added Controller class and created game package

---
+++ src/main/java/com/l09gr01/badice/controller/game/GameController.java
@@ -0,0 +1,9 @@
+package com.l09gr01.badice.controller.game;
+
+
+
+public abstract class GameController extends Controller<Arena> {
+    public GameController(Arena arena) {
+        super(arena);
+    }
+}
---
+++ src/main/java/com/l09gr01/badice/controller/game/MonsterController.java
@@ -0,0 +1,4 @@
+package com.l09gr01.badice.controller.game;
+
+public class MonsterController {
+}
