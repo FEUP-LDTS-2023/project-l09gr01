b560faafb1807ec0111eb76a409540c5e47616e4
Luciano <luciano2607ferreira@gmail.com>
2023-11-26 16:57:36 +0000

    fiz testes do GUI inputhandler, mas e melhor evermos mais disto dps

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.googlecode.lanterna:lanterna:3.1.1")
+    testImplementation("org.mockito:mockito-core:3.7.7")
 }
 
 tasks.test {
